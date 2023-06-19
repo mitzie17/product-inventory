ba297227b498791b6fc9efcba6f2c4f77138f9ec
Mitzi17 <m_estrada17@live.com>
2023-06-19 15:37:10 -0700

    added CRUD methods

--- app.js
+++ app.js
@@ -9,4 +9,33 @@ class Article {
 
 class ArticleInventory {
     static url = 'https://6490c6d22f2c7ee6c2c77d0d.mockapi.io/api/v1/articles'
+
+    static getAllArticles() {
+        return $.get(this.url);
+    }
+
+    static getArticle(id) {
+        return $.get(this.url + `/${id}`);
+    }
+
+    static createArticle(article) {
+        return $.post(this.url, article);
+    }
+
+    static updateArticle(article) {
+        return $.ajax({
+            url: this.url + `/${article._id}`,
+            dataType: 'json',
+            data: JSON.stringify(article),
+            contentTpye: 'application/json',
+            type: 'PUT'
+        });
+    }
+
+    static deleteArticle(id) {
+        return $.ajax({
+            url: this.url + `/${id}`,
+            type: 'DELETE'
+        });
+    }
 }
