f8f67fc0045e9f8c0ea6a753e5ea95c5e703d67c
Mitzi17 <m_estrada17@live.com>
2023-06-23 17:34:58 -0700

    made changes to editArticle and updateArticle methods

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 class Article {
     constructor(name, subfamily, price, units, image) {
         this.name = name;
-        this.subfamily = subfamily;
         this.price = price;
-        this.units = units;
-        this.image = image
+        this.subfamily = subfamily;
+        this.image = image;
+        this.units = units  
     }
 }
 
@@ -20,17 +20,25 @@ class ArticleInventory {
     }
 
     static createArticle(article) {
+        console.log(article)
         return $.post(this.url, article);
     }
 
-    static updateArticle(article) {
+    static updateArticle(id, article) {
+        console.log(id)
+        console.log(article)
         return $.ajax({
-            url: this.url + `/${article._id}`,
+            url: this.url + `/${id}`,
             dataType: 'json',
-            data: JSON.stringify(article),
+            data: JSON.stringify([article]),
             contentTpye: 'application/json',
             type: 'PUT'
-        });
+        }).then(() => {
+            return ArticleInventory.getAllArticles();
+        })
+            .then((articles) => {
+                DOMManager.render(articles);
+            });
     }
 
     static deleteArticle(id) {
@@ -58,12 +66,22 @@ class DOMManager {
         .then((articles) => this.render(articles));
     }
 
-    static editArticle(name, subfamily, price, units, image) {
-        ArticleInventory.updateArticle(name, subfamily, price, units, image)
+    static editArticle(id) {
+        console.log(id)
+        let updatedArticle = {
+                    "name": $(`#edit-article-name-${id}`).val(),
+                    "price": $(`#edit-article-price-${id}`).val(),
+                    "subfamily": $(`#edit-article-subfamily-${id}`).val(),
+                    "image": $(`#edit-article-image-${id}`).val(),
+                    "units": $(`#edit-article-units-${id}`).val(),
+                    "id": id
+                };
+        
+        ArticleInventory.updateArticle(id, updatedArticle)
         .then(() => {
             return ArticleInventory.getAllArticles();
         })
-        .then((articles) => this.render(this.articles));
+        .then((articles) => this.render(articles));
     }
 
     static deleteArticle(id) {
@@ -84,20 +102,21 @@ class DOMManager {
                         <h2 id="article-name">${article.name}</h2>
                         
                         <button class="btn btn-danger" onclick="DOMManager.deleteArticle('${article.id}')">Delete</button>
-                        <button class="btn btn-success edit-article-btn" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                        Edit
+
+                        <button class="btn btn-success edit-article-btn" data-bs-toggle="collapse" data-bs-target="#collapse-${article.id}" aria-expanded="false" aria-controls="collapseExample">
+                        Edit-${article.id}
                         </button>
-                            <div class="collapse" id="collapseExample">
+                            <div class="collapse" id="collapse-${article.id}">
                             
-                            <div id="edit-article-div" >
+                            <div id="edit-article-${article.id}-div" >
                             <h2 id="edit-article-heading">Edit Article</h2>
-                            <input type="text" id="edit-article-name" class="form-control" placeholder="Article Name"> <br>
-                            <input type="text" id="edit-article-subfamily" class="form-control" placeholder="Subfamily"> <br>
-                            <input type="number" id="edit-article-price" class="form-control" placeholder="Price"> <br>
-                            <input type="number" id="edit-article-units" class="form-control" placeholder="Units"> <br>
-                            <input type="text" id="edit-article-image" class="form-control" placeholder="Image URL"> <br>
+                            <input type="text" id="edit-article-name-${article.id}" class="form-control" value="${article.name}"> <br>
+                            <input type="text" id="edit-article-subfamily-${article.id}" class="form-control" value="${article.subfamily}"> <br>
+                            <input type="number" id="edit-article-price-${article.id}" class="form-control" value="${article.price}"> <br>
+                            <input type="number" id="edit-article-units-${article.id}" class="form-control" value="${article.units}"> <br>
+                            <input type="text" id="edit-article-image-${article.id}" class="form-control" value="${article.image}"> <br>
                             
-                            <button id="edit-article-btn" class="btn btn-success form-control">Submit</button>
+                            <button class="btn btn-success form-control onclick="DOMManager.editArticle('${article.id}')">Submit-${article.id}</button>
                             </div>
                             
                             </div>
@@ -136,18 +155,37 @@ $('#create-new-article').click(() => {
     $('#new-article-image').val('');
 });
 
-$('#edit-article-btn').click(() => {
-    DOMManager.editArticle($('#edit-article-name').val(),
-    $('#edit-article-subfamily').val(),
-    $('#edit-article-price').val(),
-    $('#edit-article-units').val(),
-    $('#edit-article-image').val());
+// $(`#edit-article-btn`).click(() => {
+//     let updatedArticle = {
+//         "name": $(`#edit-article-name-${id}`).val(),
+//         "price": $(`#edit-article-price-${id}`).val(),
+//         "subfamily": $(`#edit-article-subfamily-${id}`).val(),
+//         "image": $(`#edit-article-image-${id}`).val(),
+//         "units": $(`#edit-article-units-${id}`).val(),
+//         "id": id
+//     };
 
-    $('#edit-article-name').val('');
-    $('#edit-article-subfamily').val('');
-    $('#edit-article-price').val('');
-    $('#edit-article-units').val('');
-    $('#edit-article-image').val('');
-});
+//     $(`#edit-article-name-${id}`).val('');
+//     $(`#edit-article-subfamily-${id}`).val('');
+//     $(`#edit-article-price-${id}`).val('');
+//     $(`#edit-article-units-${id}`).val('');
+//     $(`#edit-article-image-${id}`).val('');
+
+//     DOMManager.editArticle(id, updatedArticle)
+// })
+
+// $('#edit-article-btn').click(() => {
+//     DOMManager.editArticle($('#edit-article-name').val(),
+//     $('#edit-article-subfamily').val(),
+//     $('#edit-article-price').val(),
+//     $('#edit-article-units').val(),
+//     $('#edit-article-image').val());
+
+//     $('#edit-article-name').val('');
+//     $('#edit-article-subfamily').val('');
+//     $('#edit-article-price').val('');
+//     $('#edit-article-units').val('');
+//     $('#edit-article-image').val('');
+// });
 
 DOMManager.getAllArticles();
