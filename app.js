91a1304c42e2afb7c73dabc8f730349f7e2b8e26
Mitzi17 <m_estrada17@live.com>
2023-06-25 22:30:55 -0700

    added comments

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
+//const { data } = require("jquery");
+
+// Attributes that an article has when created
 class Article {
     constructor(name, subfamily, price, units, image) {
         this.name = name;
         this.price = price;
         this.subfamily = subfamily;
         this.image = image;
-        this.units = units  
+        this.units = units;  
     }
 }
 
 class ArticleInventory {
+    // URL where reguests are sent
     static url = 'https://6490c6d22f2c7ee6c2c77d0d.mockapi.io/api/v1/articles'
 
+    // Gets all article objects in the database
     static getAllArticles() {
         return $.get(this.url);
     }
 
+    // Gets a specific article by its id
     static getArticle(id) {
         return $.get(this.url + `/${id}`);
     }
 
+    // Creates an article
     static createArticle(article) {
-        console.log(article)
         return $.post(this.url, article);
     }
 
+    // This is where the update is suppose to occur, however AJAX PUT request doesn't update the article in the database.
     static updateArticle(id, article) {
+    // Console logging id and article does return the right article id and article object, however...
         console.log(id)
         console.log(article)
+
+        $(`#editArticle_${id}`).show();
+        $(`#${id} p`).hide();
+    // Somewhere here is where the article object doesn't get updated in the database. Did some research but could not find a way for 
+    // the newly edited article to re-render with the new edited information. Currently still working on this....
         return $.ajax({
             url: this.url + `/${id}`,
             dataType: 'json',
-            data: JSON.stringify([article]),
+            data: JSON.stringify(article),
             contentTpye: 'application/json',
+            crossDomain: true,
             type: 'PUT'
         }).then(() => {
             return ArticleInventory.getAllArticles();
@@ -41,6 +55,7 @@ class ArticleInventory {
             });
     }
 
+    // Deletes an article by passing its id
     static deleteArticle(id) {
         return $.ajax({
             url: this.url + `/${id}`,
@@ -52,12 +67,15 @@ class ArticleInventory {
 class DOMManager {
     static articles;
     
+    // Renders all articles when called
     static getAllArticles() {
         ArticleInventory.getAllArticles()
         .then(articles =>
             this.render(articles));
     }
 
+    // Creates article by calling the constructor and passing all attributes associated with an article object
+    // It also renders all articles
     static createArticle(name, subfamily, price, units, image) {
         ArticleInventory.createArticle(new Article(name, subfamily, price, units, image))
         .then(() => {
@@ -66,24 +84,7 @@ class DOMManager {
         .then((articles) => this.render(articles));
     }
 
-    static editArticle(id) {
-        console.log(id)
-        let updatedArticle = {
-                    "name": $(`#edit-article-name-${id}`).val(),
-                    "price": $(`#edit-article-price-${id}`).val(),
-                    "subfamily": $(`#edit-article-subfamily-${id}`).val(),
-                    "image": $(`#edit-article-image-${id}`).val(),
-                    "units": $(`#edit-article-units-${id}`).val(),
-                    "id": id
-                };
-        
-        ArticleInventory.updateArticle(id, updatedArticle)
-        .then(() => {
-            return ArticleInventory.getAllArticles();
-        })
-        .then((articles) => this.render(articles));
-    }
-
+    // Passes an article id to the deleteArticle method to delete that article and renders all articles
     static deleteArticle(id) {
         ArticleInventory.deleteArticle(id)
         .then(() => {
@@ -92,6 +93,7 @@ class DOMManager {
         .then((articles) => this.render(articles));
     }
 
+    // Method that renders all articles in the database
     static render(articles) {
         this.articles = articles;
         $('#app').empty();
@@ -102,24 +104,18 @@ class DOMManager {
                         <h2 id="article-name">${article.name}</h2>
                         
                         <button class="btn btn-danger" onclick="DOMManager.deleteArticle('${article.id}')">Delete</button>
+                        <button class="btn btn-success" id="updateArticle" onclick="DOMManager.updateArticle('${article.id}')">Edit</button>
 
-                        <button class="btn btn-success edit-article-btn" data-bs-toggle="collapse" data-bs-target="#collapse-${article.id}" aria-expanded="false" aria-controls="collapseExample">
-                        Edit-${article.id}
-                        </button>
-                            <div class="collapse" id="collapse-${article.id}">
-                            
-                            <div id="edit-article-${article.id}-div" >
-                            <h2 id="edit-article-heading">Edit Article</h2>
-                            <input type="text" id="edit-article-name-${article.id}" class="form-control" value="${article.name}"> <br>
-                            <input type="text" id="edit-article-subfamily-${article.id}" class="form-control" value="${article.subfamily}"> <br>
-                            <input type="number" id="edit-article-price-${article.id}" class="form-control" value="${article.price}"> <br>
-                            <input type="number" id="edit-article-units-${article.id}" class="form-control" value="${article.units}"> <br>
-                            <input type="text" id="edit-article-image-${article.id}" class="form-control" value="${article.image}"> <br>
-                            
-                            <button class="btn btn-success form-control onclick="DOMManager.editArticle('${article.id}')">Submit-${article.id}</button>
-                            </div>
-                            
-                            </div>
+
+                        <div id="editArticle_${article.id}" class="mt-2" style="display:none;">
+                            <input type="text" id="editName_${article.id}" class="form-control m-1" value="${article.name}">
+                            <input type="text" id="editSubfamily_${article.id}" class="form-control m-1" value="${article.subfamily}">
+                            <input type="number" id="editPrice_${article.id}" class="form-control m-1" value="${article.price}">
+                            <input type="number" id="editUnits_${article.id}" class="form-control m-1" value="${article.units}">
+                            <input type="text" id="editImage_${article.id}" class="form-control m-1" value="${article.image}">
+                            <button class="btn btn-success m-1" onclick="DOMManager.saveArticle('${article.id}')">Save</button>
+                        </div>
+
                     </div>
 
                     <div class="card-body">
@@ -139,8 +135,33 @@ class DOMManager {
             );
         }
     }
+
+    // Displays the input fields for updating an article 
+    static updateArticle(id) {
+        $(`#editArticle_${id}`).show();
+        $(`#${id} p`).hide();
+    }
+
+    // This method captures the values in the edit fields and updates the article in the API and re-renders the articles
+    static saveArticle(id) {
+        let articleUpdated = {
+            "name": $(`#editName_${id}`).val(),
+            "price": $(`#editPrice_${id}`).val(),
+            "subfamily": $(`#editSubfamily_${id}`).val(),
+            "units": $(`#editUnits_${id}`).val(),
+            "image": $(`#editImage_${id}`).val(),
+            "id": id
+        }
+        ArticleInventory.updateArticle(id, articleUpdated)
+            .then(() => {
+                return ArticleInventory.getAllArticles();
+            })
+            .then((articles) => this.render(articles));
+    }
 }
 
+// Event listener attached to the create button that initiates the creating process of a new article object
+// Passes all values in the input fields to the createArticle method as arguments
 $('#create-new-article').click(() => {
     DOMManager.createArticle($('#new-article-name').val(),
     $('#new-article-subfamily').val(),
@@ -155,37 +176,5 @@ $('#create-new-article').click(() => {
     $('#new-article-image').val('');
 });
 
-// $(`#edit-article-btn`).click(() => {
-//     let updatedArticle = {
-//         "name": $(`#edit-article-name-${id}`).val(),
-//         "price": $(`#edit-article-price-${id}`).val(),
-//         "subfamily": $(`#edit-article-subfamily-${id}`).val(),
-//         "image": $(`#edit-article-image-${id}`).val(),
-//         "units": $(`#edit-article-units-${id}`).val(),
-//         "id": id
-//     };
-
-//     $(`#edit-article-name-${id}`).val('');
-//     $(`#edit-article-subfamily-${id}`).val('');
-//     $(`#edit-article-price-${id}`).val('');
-//     $(`#edit-article-units-${id}`).val('');
-//     $(`#edit-article-image-${id}`).val('');
-
-//     DOMManager.editArticle(id, updatedArticle)
-// })
-
-// $('#edit-article-btn').click(() => {
-//     DOMManager.editArticle($('#edit-article-name').val(),
-//     $('#edit-article-subfamily').val(),
-//     $('#edit-article-price').val(),
-//     $('#edit-article-units').val(),
-//     $('#edit-article-image').val());
-
-//     $('#edit-article-name').val('');
-//     $('#edit-article-subfamily').val('');
-//     $('#edit-article-price').val('');
-//     $('#edit-article-units').val('');
-//     $('#edit-article-image').val('');
-// });
-
+// Calls the method to render (display) all articles when the page loads
 DOMManager.getAllArticles();
